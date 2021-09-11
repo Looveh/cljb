7be9ffc0f0a867c97627865a01d673411ed68c9b
Niclas Nilsson <niclas.nilsson@protonmail.com>
2021-09-11 18:04:22 +0200

    Prompt

--- src/cljb/main.cljs
+++ src/cljb/main.cljs
@@ -3,11 +3,17 @@
             [reagent.dom :as rd]
             [sci.core :as sci]))
 
+(declare my-eval)
+
+(defn prompt []
+  (my-eval (js/prompt "Insert cljs here")))
+
 (def ctx
   (sci/init {:classes {'js goog/global :allow :all}
              :namespaces {'reagent.core {'atom r/atom}
                           'reagent.dom {'render rd/render}
-                          'clojure.core {'println println}}}))
+                          'clojure.core {'println println
+                                         'prompt prompt}}}))
 
 (defn my-eval [s]
   (sci/eval-string* ctx s))
